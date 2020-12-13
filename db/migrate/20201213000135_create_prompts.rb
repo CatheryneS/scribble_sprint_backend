db65069dd317dc4df8797491f2f3c9939c36f749
Catheryn Small <catheryn.small@gmail.com>
2020-12-13 00:49:26 -0500

    update migration

--- db/migrate/20201213000135_create_prompts.rb
+++ db/migrate/20201213000135_create_prompts.rb
@@ -2,7 +2,7 @@ class CreatePrompts < ActiveRecord::Migration[5.2]
   def change
     create_table :prompts do |t|
       t.string :category
-      t.string :options
+      t.string :description
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-fw = Notebook.create(name: "Freewrite")
-rwp = Notebook.create(name: "Random Word Stories")
-flp = Notebook.create(name: "First Line Stories")
-dtp = Notebook.create(name: "Deep Thinkers")
+Notebook.create(name: "Freewrite")
+Notebook.create(name: "Random Word Stories")
+Notebook.create(name: "First Line Stories")
+Notebook.create(name: "Deep Thinkers")
+
+Prompt.create(description: "Freewrite", category:"No Prompt")
+Prompt.create(description: "What gets you excited about life?", category: "Deep Thinkers")
+Prompt.create(description: "What life lesson did you learn the hard way?", category: "Deep Thinkers")
+Prompt.create(description: "Footsteps slowly creaked on every step of the stairs. The bedroom door handle turned slowly.", category: "First Line Stories")
+Prompt.create(description: "A shrill cry echoed in the mist", category: "First Line Stories")
